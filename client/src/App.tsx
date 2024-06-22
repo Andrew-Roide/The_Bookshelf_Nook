280a98c6253618deadf746efd50e293ff9fab5dd
Andrew-Roide <andrew.roide@hotmail.com>
2024-06-22 03:24:49 +0000

    removed unnecessary code from initial set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { BookInfo } from '../../shared/BookInfo';
 import NavBar from './NavBar';
 import HomePage from './HomePage';
 import SearchResults from './SearchResults';
@@ -8,26 +6,14 @@ import SavedBooks from './SavedBooks';
 import './App.css';
 
 export default function App() {
-  const [savedBooks, setSavedBooks] = useState<BookInfo[]>([]);
-
-  const addBookToLibrary = (book: BookInfo) => {
-    setSavedBooks((prevBooks) => [...prevBooks, book]);
-  };
-
   return (
     <>
       <Router>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route
-            path="/search-results"
-            element={<SearchResults addBookToLibrary={addBookToLibrary} />}
-          />
-          <Route
-            path="/saved-books"
-            element={<SavedBooks savedBooks={savedBooks} />}
-          />
+          <Route path="/search-results" element={<SearchResults />} />
+          <Route path="/saved-books" element={<SavedBooks />} />
         </Routes>
       </Router>
     </>
