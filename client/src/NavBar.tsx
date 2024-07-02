9ddcc5929eebcc6e6862b3547ef35ccf033503a9
Andrew Roide <andrew.roide@hotmail.com>
2024-07-02 11:29:14 -0700

    Revert "added hamburger menu for mobile devices, added favicon icon"

--- client/src/NavBar.tsx
+++ client/src/NavBar.tsx
@@ -1,72 +1,36 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-
   function handleSignOut() {
     sessionStorage.removeItem('token');
   }
 
   return (
-    <header>
-      <div className="max-w-full my-12 flex justify-between items-center px-4">
-        <div className="text-5xl font-slab font-bold text-customGreen text-shadow-customNav">
-          <Link to="/">
-            <h1>The Bookshelf Nook</h1>
-          </Link>
-        </div>
-        <div className="flex items-center">
-          <div className="hidden md:flex gap-10 text-2xl font-slab font-bold text-customGreen text-shadow-customNav">
-            <Link to="/" className="navbar-link">
-              Find A Book
-            </Link>
-            <Link to="/saved-books" className="navbar-link">
-              Your Booknook
-            </Link>
-            <Link to="/register">Register</Link>
-            <Link to="/sign-in">Sign In</Link>
-            <Link to="/" onClick={handleSignOut}>
-              Sign Out
+    <>
+      <header>
+        <div className="max-w-full my-12 flex justify-between items-center">
+          <div className="text-5xl font-slab font-bold text-customGreen text-shadow-customNav">
+            <Link to="/">
+              <h1>The Bookshelf Nook</h1>
             </Link>
           </div>
-          <button
-            className="md:hidden text-customGreen focus:outline-none"
-            onClick={() => setIsOpen(!isOpen)}>
-            <svg
-              className="w-8 h-8"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={
-                  isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'
-                }></path>
-            </svg>
-          </button>
+          <div className="">
+            <nav className="font-slab font-bold flex justify-between gap-10 text-2xl text-customGreen text-shadow-customNav">
+              <Link to="/" className="navbar-link">
+                Find A Book
+              </Link>
+              <Link to="/saved-books" className="navbar-link">
+                Your Booknook
+              </Link>
+              <Link to="/register">Register</Link>
+              <Link to="/sign-in">Sign In</Link>
+              <Link to="/sign-in" onClick={handleSignOut}>
+                Sign Out
+              </Link>
+            </nav>
+          </div>
         </div>
-      </div>
-      {isOpen && (
-        <div className="md:hidden bg-customBrown shadow-lg rounded-lg px-4 py-6">
-          <nav className="flex flex-col gap-4 text-xl font-slab font-bold text-customGreen text-shadow-customNav">
-            <Link to="/" className="navbar-link">
-              Find A Book
-            </Link>
-            <Link to="/saved-books" className="navbar-link">
-              Your Booknook
-            </Link>
-            <Link to="/register">Register</Link>
-            <Link to="/sign-in">Sign In</Link>
-            <Link to="/" onClick={handleSignOut}>
-              Sign Out
-            </Link>
-          </nav>
-        </div>
-      )}
-    </header>
+      </header>
+    </>
   );
 }
